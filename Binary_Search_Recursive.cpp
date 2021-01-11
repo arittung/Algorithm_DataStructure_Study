06fac3c548078a664d2e75251f108d3c104e1821
길다영 <53934639+arittung@users.noreply.github.com>
2021-01-11 16:34:02 +0900

    Update Binary_Search_Recursive.cpp

--- Binary_Search_Recursive.cpp
+++ Binary_Search_Recursive.cpp
@@ -2,30 +2,30 @@
 using namespace std;
 
 int bresearch(int* arr, int first, int last, int target) {
-	int mid;
-	while (first <= last) {
+	int mid= 0;
+	if (first > last) {
 		mid = (first + last) / 2;
-		if (target == arr[mid]) {
-			return mid;
+	}
+
+	if (target == arr[mid]) {
+		return mid;
+	}
+	else {
+		if (target < arr[mid]) {
+			return bresearch(arr, first, mid - 1, target);
 		}
 		else {
-			if (target < arr[mid]) {
-				return bresearch(arr, first, mid - 1, target);
-			}
-			else {
-				return bresearch(arr, mid + 1, last, target);
-			}
+			return bresearch(arr, mid + 1, last, target);
 		}
-		return -1;
 	}
+	return -1;
 }
 
-
 int main() {
-	int arr[] = { 1, 3, 5, 7, 9 };
+	int arr[] = { 1, 3, 5, 7, 9 ,11, 13};
 	int length = sizeof(arr) / sizeof(int);
 	int idx;
-	idx = bresearch(arr, 0, length - 1, 7);
+	idx = bresearch(arr, 0, length - 1, 5);
 
 	if (idx == -1) {
 		cout << "Fail to search!" << endl;

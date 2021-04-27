2a72b462b7c16084562435f6d2f6ca9d0de1f682
Dayoung <rlfekdud1139@naver.com>
2021-04-27 21:05:47 +0900

    change deque

--- Deque/Deque.cpp
+++ Deque/Deque.cpp
@@ -96,4 +96,18 @@ class CircularDeque{
         
 };
 
+int main() {
+    CircularDeque deq;
+    for (int i = 1; i < 10; i++) {
+        if (i % 2) deq.addFront(i);
+        else deq.addRear(i);
+    }
+    deq.display();
+    deq.deleteFront();
+    deq.deleteRear();
+    deq.deleteFront();
+    deq.display();
+    return 0;
+}
+
 

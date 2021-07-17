9729ac1fd4defd012101d2c9f91bc53450413df0
Dayoung Kil <53934639+arittung@users.noreply.github.com>
2021-07-18 02:12:53 +0900

    Update fibo_dynamic.cpp

--- algorithm/Dynamic Programming/fibo_dynamic.cpp
+++ algorithm/Dynamic Programming/fibo_dynamic.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-
 using namespace std;
 
+long long int fibo_memo[100];
+
 long long int fibo_daynamic(long long int n) {
-	static long long int fibo_memo[] = { 0,0 };
+
 	if (n<= 1) {
 		return n;
 	}
@@ -12,15 +13,15 @@ long long int fibo_daynamic(long long int n) {
 	}
 	else {
 		fibo_memo[n] = fibo_daynamic(n - 1) + fibo_daynamic(n - 2);
+
+
+		return fibo_memo[n];
 	}
-	
-	
-	return fibo_memo[n];
 }
 
 int main() {
 	
-	int n = 100;
+	int n = 3;
 	
 	cout << fibo_daynamic(n) << endl;
 
